d34c22566d0e90790c2603fe2f42f9e8db846004
Takafumi Arakaki <aka.tkf@gmail.com>
2021-05-19 19:41:58 -0700

    Add documentation and doctest (#6)

--- src/StopWatches.jl
+++ src/StopWatches.jl
@@ -1,22 +1,3 @@
-"""
-    StopWatches
-
-Simple intrusive time measurements.
-
-# Measurement API
-
-* `@stopwatch([tag], code)`: measure the times to execute `code`.
-* `StopWatches.clear()`: reset timing counters.
-
-Measurement API is also available from `StopWatchesBase` package.  `StopWatches`
-simply re-expose these APIs.
-
-# Analysis API
-
-* `StopWatches.summary()`: display summary data.
-* `StopWatches.time_dataframe()`: export summary data as a `DataFrame`.
-* `StopWatches.time_table()`: export summary data as a table.
-"""
 baremodule StopWatches
 
 export @stopwatch
@@ -37,8 +18,11 @@ using Unitful: @u_str, uconvert
 
 using ..StopWatches: StopWatches
 
+include("utils.jl")
 include("conversions.jl")
 
 end  # module Implementations
+
+Implementations.define_docstrings()
 
 end  # baremodule StopWatches
